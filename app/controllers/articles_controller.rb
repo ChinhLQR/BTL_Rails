97c0bb3a8d336aeed4af5f32077623d4ae3d0c40
ChinhLQ <chinhhedspibk@gmail.com>
2017-12-19 23:28:03 +0700

    notice

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
         @article = Article.new(article_params)
        
         if @article.save    
-            flash[:notice]= "Article was successfully created"
+            flash[:success]= "Article was successfully created"
             redirect_to article_path(@article)
         else
             render 'new'
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
     def update
        
         if @article.update(article_params)
-            flash[:notice] = "Update success"
+            flash[:success] = "Update success"
             redirect_to article_path(@article)
         else
             render 'edit'
@@ -42,7 +42,7 @@ class ArticlesController < ApplicationController
     def destroy
         
         @article.destroy
-        flash[:notice] = "Delete success"
+        flash[:danger] = "Delete success"
         redirect_to articles_path
     end
 
