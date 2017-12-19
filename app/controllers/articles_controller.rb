3b06f993f6a53f49a2ba97dc930f4181297d6b2e
ChinhLQ <chinhhedspibk@gmail.com>
2017-12-19 19:16:27 +0700

    Add edit update show index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,16 @@
 class ArticlesController < ApplicationController
+    
+    def index
+        @articles = Article.all
+    end
+    
+    
     def new
         @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
     end
 
     def create
@@ -18,6 +28,15 @@ class ArticlesController < ApplicationController
         end
     end
 
+    def update
+        @article =Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Update success"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
 
     def show
         @article = Article.find(params[:id])
