5afecdc9bb1c33756795154a2184aa7e6849a6ce
ChinhLQ <chinhhedspibk@gmail.com>
2017-12-24 08:28:17 +0700

    xong authenti

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,10 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:edit, :update, :show, :destroy ]
+    before_action :require_user, except: [:index, :show]
+    before_action :require_same_user, only: [:edit,:update,:destroy]
+    
     def index
-        @articles = Article.paginate(page: params[:page],per_page:5)
+        @articles = Article.where('title LIKE ?', '%filler%').paginate(page: params[:page],per_page:5)
     end
     
     
@@ -52,5 +55,11 @@ class ArticlesController < ApplicationController
     end
     def article_params
         params.require(:article).permit(:title,:description)
+    end
+    def require_same_user
+        if current_user != @article.user
+            flash[:danger] = "you can only edit or delete your articles"
+            redirect_to root_path
+        end
     end
 end
